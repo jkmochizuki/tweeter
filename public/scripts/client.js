ac012b3e9478d0b107da3a8f5b431c9a038b9db3
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-02-28 15:43:40 -0800

    Implement renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,49 @@
 $(document).ready(function() {
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1677429048574
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1677515448574
+    }
+  ];
+
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const result = createTweetElement(tweet);
+      $('.tweets-container').append(result);
+    }
   };
 
-  const createTweetElement = (data) => {
-    const result = $(`<article class="tweet">
+  const createTweetElement = (tweets) => {
+    const $tweet = $(`<article class="tweet">
     <header>
       <div class="user">
-        <img src=${data.user.avatars}>
-        <p>${data.user.name}</p>
+        <img src=${tweets.user.avatars}>
+        <p>${tweets.user.name}</p>
       </div>
-      <div class="username">${data.user.handle}</div>
+      <div class="username">${tweets.user.handle}</div>
     </header>
-    <h4 id="tweets-text">${data.content.text}</h4>
+    <h4 id="tweets-text">${tweets.content.text}</h4>
     <footer>
-      <span class="">${data.created_at}</span>
+      <span class="">${tweets.created_at}</span>
       <span class="right-corner-icons">
         <i class="fa-sharp fa-solid fa-flag"></i> 
         <i class="fa-solid fa-retweet"></i> 
@@ -32,13 +51,9 @@ $(document).ready(function() {
       </span>
     </footer>
   </article>`);
-    return result;
+    return $tweet;
   };
 
-  const $tweet = createTweetElement(tweetData);
-
-  // // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('.tweets-container').append($tweet);
-
+  renderTweets(tweetData);
+  
 });
